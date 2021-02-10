e089e731333da8cd985be269188a477970dac60a
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-10 16:45:02 -0300

    test: deve interagir com checkbox

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
@@ -7,11 +8,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteCampoTreinamento {
 	
-	@Test
-	public void teste() {
-		WebDriver driver = new ChromeDriver();
+	private WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		driver = new ChromeDriver();
 		driver.manage().window().setSize(new Dimension(1200, 765));
 		driver.get("file://" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	}
+	
+	@Test
+	public void teste() {
 		driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste do sucesso");
 		Assert.assertEquals("Teste do sucesso",  driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
 		
@@ -21,11 +28,24 @@ public class TesteCampoTreinamento {
 	
 	@Test
 	public void deveInteragirComTextArea() {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setSize(new Dimension(1200, 765));
-		driver.get("file://" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Teste sugestão");
-		Assert.assertEquals("Teste sugestão",  driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Teste sugestão\n\nNova linha\nMais uma linha");
+		Assert.assertEquals("Teste sugestão\n\nNova linha\nMais uma linha",  driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void deveInteragirComRadioButton() {
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void deveInteragirComCheckbox() {
+		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
 		
 		driver.quit();
 	}
