ecc218e043b4f41b83b64476c09e5c2381f469f3
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-12 23:14:24 -0300

    refactor: adiciona método com anotação @After para finalizar driver

--- src/main/java/TesteCadastro.java
+++ src/main/java/TesteCadastro.java
@@ -1,3 +1,4 @@
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +18,11 @@ public class TesteCadastro {
 		driver = new ChromeDriver();
 		driver.manage().window().setSize(new Dimension(1200, 765));
 		driver.get("file://" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	}
+	
+	@After
+	public void finaliza() {
+		driver.quit();
 	}
 	
 	@Test
@@ -47,8 +53,6 @@ public class TesteCadastro {
 		driver.findElement(By.id("elementosForm:cadastrar")).click();
 		Alert alert = driver.switchTo().alert();
 		Assert.assertEquals("Nome eh obrigatorio", alert.getText());
-		
-		driver.quit();
 	}
 	
 	@Test
@@ -57,8 +61,6 @@ public class TesteCadastro {
 		driver.findElement(By.id("elementosForm:cadastrar")).click();
 		Alert alert = driver.switchTo().alert();
 		Assert.assertEquals("Sobrenome eh obrigatorio", alert.getText());
-		
-		driver.quit();
 	}
 	
 	@Test
@@ -68,8 +70,6 @@ public class TesteCadastro {
 		driver.findElement(By.id("elementosForm:cadastrar")).click();
 		Alert alert = driver.switchTo().alert();
 		Assert.assertEquals("Sexo eh obrigatorio", alert.getText());
-		
-		driver.quit();
 	}
 	
 	@Test
@@ -83,8 +83,6 @@ public class TesteCadastro {
 		driver.findElement(By.id("elementosForm:cadastrar")).click();
 		Alert alert = driver.switchTo().alert();
 		Assert.assertEquals("Tem certeza que voce eh vegetariano?", alert.getText());
-		
-		driver.quit();
 	}
 
 }
