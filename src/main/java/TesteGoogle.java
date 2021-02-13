d958b355bdc53129a56cc806bfeccb75f50c40f6
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-12 23:22:41 -0300

    refactor: adiciona método com anotação @After para finalizar driver na classe TestGoogle

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,4 +1,6 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
@@ -6,10 +8,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteGoogle {
 	
+	private WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("http://www.google.com");
+	}
+	
+	@After
+	public void finaliza() {
+		driver.quit();
+	}
+	
 	@Test
 	public void test() {
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.google.com");
+		
 		driver.manage().window().setSize(new Dimension(1200, 765));
 		
 		Assert.assertEquals("Google", driver.getTitle());
